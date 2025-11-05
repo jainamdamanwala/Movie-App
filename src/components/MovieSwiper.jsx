71306ba6ae2bd28dfa09f238ae04c273aa33003e
Jainam Damanwala <jainamdamanwala21@gmail.com>
2025-11-05 16:39:30 -0600

    Fixed the carousal

--- src/components/MovieSwiper.jsx
+++ src/components/MovieSwiper.jsx
@@ -2,41 +2,42 @@ import React from 'react';
 import './movieSwiper.css';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectCoverflow } from 'swiper/modules';
 
-import {Autoplay, EffectCoverflow} from 'swiper/modules';
+function MovieSwiper({ slides = [], slideChange = () => {} }) {
+  const handleSlideChange = (swiper) => {
+    const idx = swiper?.realIndex ?? 0;
+    const id = slides[idx]?._id;
+    if (id) slideChange(id);
+  };
 
-function MovieSwiper({slides, slideChange}) {
-    return ( 
-        <Swiper
-            effect={'coverflow'}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={'auto'}
-            autoplay={{
-                delay:2500,
-                disableOnInteraction:false,
-            }}
-            coverflowEffect={{
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows:true,
-            }}
-            loop={true}
-            modules={[Autoplay,EffectCoverflow]}
-            className='movieSwiper'
-        >
-            {
-                slides.map(slide=>(
-                    <SwiperSlide key={slide._id}>
-                        <img src={slide.previewImg} alt="Preview" onClick={() => slideChange(slide._id)}/>
-                    </SwiperSlide>
-                ))
-            }
-        </Swiper>
-     );
+  const handleInit = (swiper) => {
+    // fire once for the initially visible slide
+    handleSlideChange(swiper);
+  };
+
+  return (
+    <Swiper
+      effect="coverflow"
+      grabCursor
+      centeredSlides
+      slidesPerView="auto"
+      autoplay={{ delay: 10000, disableOnInteraction: false }}
+      coverflowEffect={{ rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: true }}
+      loop
+      modules={[Autoplay, EffectCoverflow]}
+      className="movieSwiper"
+      onInit={handleInit}
+      onSlideChange={handleSlideChange}
+    >
+      {slides.map((slide) => (
+        <SwiperSlide key={slide._id}>
+          <img src={slide.previewImg} alt={slide.title || 'Preview'} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
 export default MovieSwiper;
